060872019fbb2c5c7c30d33303d90342b4e0af0f
lambdalemon <lemonjameslem@protonmail.com>
2025-12-02 18:27:34 -0500

    Finished second day

--- day2/src/day2.jl
+++ day2/src/day2.jl
@@ -1,5 +1,27 @@
 module day2
 
-greet() = print("Hello World!\n")
+function load_puzzle_input() :: Vector{String}
+    contents :: String = read("puzzle_input", String)
+    return split(contents, ",")
+end
+
+function get_range(range :: String) :: Tuple{Int, Int}
+    splitted :: Vector{String} = split(range, "-") # should always be length of 2
+    return (parse(Int, splitted[1]), parse(Int, splitted[2]))
+end
+
+function has_repeated_sequence(n :: Int)
+    n_string :: String = string(n)
+    len :: Int = length(n_string)
+
+    if len % 2 != 0
+        return false
+    end
+
+    a = n_string[1:div(len, 2)]
+    b = n_string[div(len, 2) + 1:len]
+
+    return a == b
+end
 
 end # module day2
--- day2/src/main.jl
+++ day2/src/main.jl
@@ -1,2 +1,23 @@
 using day2
 
+function main()
+    puzzle_input :: Vector{String} = day2.load_puzzle_input()
+
+    invalid_id_total :: Int = 0
+
+    for range_string in puzzle_input
+        a, b :: Int = day2.get_range(range_string)
+        print("Range $a to $b\n")
+
+        for num in a:b
+            if day2.has_repeated_sequence(num)
+                print("Found invalid id: $num\n")
+                invalid_id_total += num
+            end
+        end
+    end
+
+    print("Total: $invalid_id_total\n")
+end
+
+main()
